77b1e10531d462730e7bbbd8cb4af35bae66fcc7
Calvariaa <17198186+Calvariaa@users.noreply.github.com>
2025-02-20 17:54:27 +0800

    merge xianyu's code

---
+++ Scenes/camera_3d.gd
@@ -0,0 +1,65 @@
+extends Camera3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event):
+	_update_camera(event)
+
+
+func _unhandled_input(event):
+	pass
+
+
+var mouse_pos : Vector2
+var mouse_move_flag : bool = false
+var mouse_rotate_flag : bool = false
+var mouse_move_sensitivity : float = 0.01
+var mouse_rotation_sensitivity : float = 0.02
+func _update_camera(event):
+	# 平移：按住shift和鼠标中键
+	if not mouse_rotate_flag and Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE) and Input.is_key_pressed(KEY_SHIFT):
+		if not mouse_move_flag:
+			Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
+			mouse_pos = get_viewport().get_mouse_position()
+			print(mouse_pos)
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			mouse_move_flag = true
+			
+	if mouse_move_flag:
+		if event is InputEventMouseMotion:
+			var displacement = event.relative
+
+			# Apply camera translation
+			translate(Vector3(-displacement.x * mouse_move_sensitivity, displacement.y * mouse_move_sensitivity, 0))
+		if not Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
+			Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
+			Input.warp_mouse(mouse_pos)
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			mouse_move_flag = false
+		
+	# 旋转：按住鼠标中键
+	if not mouse_move_flag and Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
+		if not mouse_rotate_flag:
+			Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
+			mouse_pos = get_viewport().get_mouse_position()
+			print(mouse_pos)
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			mouse_rotate_flag = true
+	
+	if mouse_rotate_flag:
+		if event is InputEventMouseMotion:
+			var displacement = event.relative
+
+			rotate_x(deg_to_rad(displacement.y * mouse_rotation_sensitivity))
+			rotate_y(deg_to_rad(-displacement.x * mouse_rotation_sensitivity))
+		if not Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
+			Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
+			Input.warp_mouse(mouse_pos)
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			mouse_rotate_flag = false
