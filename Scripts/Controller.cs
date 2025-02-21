996657ba133e81f9be414a28977bd1fe018ceab2
Calvariaa <17198186+Calvariaa@users.noreply.github.com>
2025-02-21 20:40:15 +0800

    add temperature dt and anomaly calc

--- Scripts/Controller.cs
+++ Scripts/Controller.cs
@@ -1,4 +1,6 @@
+using System;
 using Godot;
+using static Godot.GD;
 using _Climate.Scripts;
 
 public partial class Controller : Node
@@ -9,14 +11,25 @@ public partial class Controller : Node
     [Export] float cellSize = 0.1f;
     [Export] int Width = 100;
     [Export] int Height = 100;
+    [Export] Double Alpha = 1e-4;
     Cell[,] cells;
+    
+    // 温度分布图，变温分布图，气温距平分布图
+    private enum MapType
+    {
+        TemperatureDistribution,
+        TemperatureChangeDistribution,
+        TemperatureAnomalyDistribution,
+        TemperatureNum
+    }
+    private MapType _mapType = MapType.TemperatureDistribution;
 
 
-    CellCalculator cellCalculator;
+    TemperatureCalculator _temperatureCalculator;
     
     public override void _Ready()
     {
-        cellCalculator = new CellCalculator(Width, Height);
+        _temperatureCalculator = new TemperatureCalculator(Width, Height, Alpha);
         cells = new Cell[Width, Height];
         cellPrefab = cellScene.Instantiate<MeshInstance3D>();
         
@@ -36,14 +49,61 @@ public partial class Controller : Node
 
     public override void _Process(double delta)
     {
-        cellCalculator.Calculate(delta);
-        for (int x = 0; x < Width; x++)
+        _temperatureCalculator.Calculate(delta);
+        
+        // 随机生成温度
+        if (Randf() < 0.1)
         {
-            for (int y = 0; y < Height; y++)
+            // cellCalculator.cells[RandRange(0, cellCalculator.width - 1), RandRange(0, cellCalculator.height - 1)]
+            //     .Temperature = RandRange(-10, 255);
+            var radius = RandRange(1, 10);
+            var width = RandRange(radius, _temperatureCalculator.Width - radius);
+            var height = RandRange(radius, _temperatureCalculator.Height - radius);
+            var temperature = RandRange(-54, 56);
+            
+            for (var x = 0; x < _temperatureCalculator.Width; x++)
             {
-                cells[x, y].Temperature = cellCalculator.cells[x, y].Temperature;
+                for (var y = 0; y < _temperatureCalculator.Height; y++)
+                {
+                    if (Mathf.Pow(x - width, 2) + Mathf.Pow(y - height, 2) < radius)
+                    {
+                        _temperatureCalculator.Cells[x, y] = temperature;
+                    }
+                }
             }
         }
+
+        switch (_mapType)
+        {
+            case MapType.TemperatureDistribution:
+                for (int x = 0; x < _temperatureCalculator.Width; x++)
+                {
+                    for (int y = 0; y < _temperatureCalculator.Height; y++)
+                    {
+                        cells[x, y].Temperature = (float)_temperatureCalculator.Cells[x, y];
+                    }
+                }
+                break;
+            case MapType.TemperatureChangeDistribution:
+                for (int x = 0; x < _temperatureCalculator.Width; x++)
+                {
+                    for (int y = 0; y < _temperatureCalculator.Height; y++)
+                    {
+                        cells[x, y].Temperature = (float)_temperatureCalculator.CellsDerivative[x, y];
+                    }
+                }
+                break;
+            case MapType.TemperatureAnomalyDistribution:
+                for (int x = 0; x < _temperatureCalculator.Width; x++)
+                {
+                    for (int y = 0; y < _temperatureCalculator.Height; y++)
+                    {
+                        cells[x, y].Temperature = (float)_temperatureCalculator.CellsAnomaly[x, y];
+                    }
+                }
+                break;
+        }
+        
     }
 
     public override void _Input(InputEvent @event)
@@ -52,7 +112,20 @@ public partial class Controller : Node
         {
             if (keyEvent.Keycode == Key.R)
             {
-                cellCalculator.ClearCells();
+                _temperatureCalculator.ClearCells();
+            }
+            
+            if (keyEvent.Keycode == Key.T)
+            {
+                Print(_mapType);
+                if (_mapType < MapType.TemperatureNum - 1)
+                {
+                    _mapType++;
+                }
+                else
+                {
+                    _mapType = 0;
+                }
             }
         }
     }
---
+++ Scripts/TemperatureCalculator.cs
@@ -0,0 +1,136 @@
+﻿using static Godot.GD;
+namespace _Climate.Scripts;
+
+public class TemperatureCalculator(int width, int height, double alpha)
+{
+    public readonly int Width = width;
+    public readonly int Height = height;
+    public readonly double Alpha = alpha;
+    public double[,] Cells = new double[width, height];
+    public double[,] CellsDerivative = new double[width, height];  // 变温分布
+    public double[,] CellsAnomaly = new double[width, height];  // 气温距平分布
+    private double[,] _cellsAverage = new double[width, height];  // 平均值
+    private uint _averageCount = 0;
+    
+    
+    public void Calculate(double delta)
+    {
+        var dx2 = 1.0 / ((Width - 1) * (Height - 1));
+
+        // 辅助函数，用于计算温度分布的导数
+        double[,] ComputeHeatEquation(double[,] cells, int width, int height, double dx2, double alpha)
+        {
+            double[,] dTdt = new double[width, height];
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    double d2Tdx2 = 0;
+                    double d2Tdy2 = 0;
+
+                    if (x > 0) d2Tdx2 += cells[x - 1, y];
+                    if (x < width - 1) d2Tdx2 += cells[x + 1, y];
+                    if (y > 0) d2Tdy2 += cells[x, y - 1];
+                    if (y < height - 1) d2Tdy2 += cells[x, y + 1];
+
+
+                    if (x == 0) d2Tdx2 += cells[width - 1, y];
+                    if (x == width - 1) d2Tdx2 += cells[0, y];
+                    if (y == 0) d2Tdy2 += cells[x, height - 1];
+                    if (y == height - 1) d2Tdy2 += cells[x, 0];
+
+                    d2Tdx2 -= 2 * cells[x, y];
+                    d2Tdy2 -= 2 * cells[x, y];
+
+                    dTdt[x, y] = alpha * (d2Tdx2 / (dx2) + d2Tdy2 / (dx2)); // 将矩阵展平成向量
+                }
+            }
+
+            return dTdt;
+        }
+
+        // https://i.imgur.com/RIlJM32.png
+        // https://zhuanlan.zhihu.com/p/8616433050
+        double[,] rk4(double[,] cells, double dt, int width, int height, double dx2, double alpha)
+        {
+            var T = new double[width, height];
+
+            // 处理内部
+            for (var x = 0; x < width; x++)
+            {
+                for (var y = 0; y < height; y++)
+                {
+                    T[x, y] = cells[x, y];
+                }
+            }
+
+            // 时间积分：使用 Runge-Kutta 方法
+            // 计算k1234
+            var k1 = ComputeHeatEquation(cells, width, height, dx2, alpha);
+            var k2 = ComputeHeatEquation(cells, width, height, dx2, alpha);
+            var k3 = ComputeHeatEquation(cells, width, height, dx2, alpha);
+            var k4 = ComputeHeatEquation(cells, width, height, dx2, alpha);
+
+            // 更新u_i^(n+1)
+            for (var x = 0; x < width; x++)
+            {
+                for (var y = 0; y < height; y++)
+                {
+                    T[x, y] += dt / 6 * (k1[x, y] + 2 * k2[x, y] + 2 * k3[x, y] + k4[x, y]);
+                }
+            }
+
+            return T;
+        }
+
+        // 数学逼提醒了我用龙格库塔法求偏微分，让我们赞美数学逼
+        var cellsUpdate = Cells;
+        var tNew = rk4(Cells, delta, Width, Height, dx2, Alpha);
+        CellsDerivative = ComputeHeatEquation(Cells, Width, Height, dx2, Alpha);
+        
+        for (var x = 0; x < Width; x++)
+        {
+            for (var y = 0; y < Height; y++)
+            {
+                cellsUpdate[x, y] = (float)tNew[x, y];
+            }
+        }
+        
+        // 距平值计算
+        for (var x = 0; x < Width; x++) 
+        {
+            for (var y = 0; y < Height; y++)
+            {
+                _cellsAverage[x, y] += (Cells[x, y] - _cellsAverage[x, y]) / (_averageCount + 1);
+            }
+        }
+
+        // 单元格的距平值
+        for (var x = 0; x < Width; x++)
+        {
+            for (var y = 0; y < Height; y++)
+            {
+                CellsAnomaly[x, y] = Cells[x, y] - _cellsAverage[x, y];
+            }
+        }
+    
+        if(_averageCount < 1000)
+            _averageCount ++;
+        
+        Cells = cellsUpdate;
+    }
+    
+    public void ClearCells()
+    {
+        for (var x = 0; x < Width; x++)
+        {
+            for (var y = 0; y < Height; y++)
+            {
+                Cells[x, y] = 0;
+                // CellsDerivative[x, y] = 0;
+                CellsAnomaly[x, y] = 0;
+                _cellsAverage[x, y] = 0;
+            }
+        }
+    }
+}
