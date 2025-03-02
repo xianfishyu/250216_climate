d2fbfd7ee981e508e72574477a06ed46338feeb2
Calvaria <1764203060@qq.com>
2025-03-02 19:38:41 +0800

    remove

--- Scripts/Earth.cs
+++ Scripts/Earth.cs
@@ -8,16 +8,16 @@ using static Tool;
 
 public class Earth
 {
-    int 远日点;
-    int 近日点;
+	int 远日点;
+	int 近日点;
 
-    int 公转时间;
-    int 自转时间;
-    int 转轴倾角;
-    int 轨道倾角;
+	int 公转时间;
+	int 自转时间;
+	int 转轴倾角;
+	int 轨道倾角;
 
-    public static float 半径 = 10;
-    int 表面积;
+	public static float 半径 = 10;
+	int 表面积;
 
-    int 地球没平方米光强度;
+	int 地球没平方米光强度;
 }
