fce1ea3f058a70ba6524ae1df6f5b9d949186e7a
xianfishyu <1371866605@qq.com>
2025-03-02 21:06:17 +0800

    #申必小推送

--- Scripts/SphereCamera3D.cs
+++ Scripts/SphereCamera3D.cs
@@ -23,6 +23,22 @@ public partial class SphereCamera3D : Camera3D
 		}
 	}
 
+	[Export]
+	private Vector2 CameraGeoCoord
+	{
+		get => new(MathF.Atan2(Position.X, -Position.Z),
+				MathF.Atan2(Position.Y, MathF.Sqrt(MathF.Pow(Position.X, 2) + MathF.Pow(Position.Z, 2))));
+		set
+		{
+			if (value.Y > 1.552555555555556)
+				value.Y = 1.552555555555556f;
+			Position = new Vector3(
+				MathF.Cos(value.Y) * MathF.Sin(value.X),
+				MathF.Sin(value.Y),
+				-MathF.Cos(value.Y) * MathF.Cos(value.X)) * CameraScale * 半径;
+		}
+	}
+
 	public override void _Ready()
 	{
 
@@ -33,8 +49,7 @@ public partial class SphereCamera3D : Camera3D
 		ScaleUpdate();
 		MouseMoveUpdate();
 
-		Rotation = new Quaternion(Vector3.Forward, -Position.Normalized()).Normalized().GetEuler();
-		Rotation = new(Rotation.X, Rotation.Y, 0);
+		LookAt(Vector3.Zero);
 
 	}
 
@@ -42,11 +57,28 @@ public partial class SphereCamera3D : Camera3D
 	{
 		Vector2 deltaPos = mousePos - GetViewport().GetMousePosition();
 		mousePos = GetViewport().GetMousePosition();
+
+		//你是看不懂这段代码的
 		if (Input.IsMouseButtonPressed(MouseButton.Left))
 		{
-			Print(deltaPos);
-			Quaternion quaternion = Quaternion.FromEuler(new Vector3(0, deltaPos.X * (CameraScale - 0.8f) / 1000, deltaPos.Y * (CameraScale - 0.8f) / 1000));
-			Position = quaternion * Position;
+			// Vector3 Vxz = new Vector3(Position.X, 0, Position.Z).Normalized();
+			// Vector3 Nor = Vxz.Cross(Vector3.Down).Normalized();
+			// Quaternion quaternion = Quaternion.FromEuler(new Vector3
+			// (Nor.X * deltaPos.Y * (CameraScale - 0.8f) / 1000, deltaPos.X * (CameraScale - 0.8f) / 1000, Nor.Z * deltaPos.Y * (CameraScale - 0.8f) / 1000));
+
+			// Vector3 newPos = (quaternion * Position.Normalized()).Normalized();
+			// float r = Position.Length() / Position.Normalized().Length();
+
+			// if (new Vector2(newPos.X, newPos.Z).Length() < 0.3f)
+			// {
+			// 	Quaternion reverse = Quaternion.FromEuler(Nor * 0.01f);
+			// 	newPos = -reverse * newPos;
+			// }
+			// Position = newPos * r;
+
+			CameraGeoCoord -= deltaPos / Mathf.Tau / 100 * (CameraScale -0.9f);
+			Print(CameraGeoCoord);
+
 		}
 	}
 
